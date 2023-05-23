436e1c23db69867eea4c2e514b7346b06b86e649
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-05-23 16:03:04 -0400

    feat: config CLI

---
+++ src/sesg_cli/cli/config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import typer
+
+from sesg_cli.config import Config
+
+app = typer.Typer(rich_markup_mode="markdown", help="Create the `config.toml` file.")
+
+
+@app.command()
+def init(
+    path: Path = typer.Argument(
+        Path.cwd() / "config.toml",
+        help="Path to where the configuration file will be created.",  # noqa: E501
+        dir_okay=False,
+        file_okay=False,
+        exists=False,
+    )
+):
+    """Initializes a `config.toml` file."""
+    default_config = Config.create_default()
+
+    default_config.to_toml(path)
+
+
+if __name__ == "__main__":
+    app()
