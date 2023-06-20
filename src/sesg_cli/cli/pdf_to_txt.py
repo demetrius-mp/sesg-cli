811f8ece7712fedb42669222bbdfb607f77ff0de
Marialuisabcs <maria.lbcs@icloud.com>
2023-06-20 02:10:36 -0400

    feat: pdf to txt converter

---
+++ src/sesg_cli/cli/pdf_to_txt.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+
+import PyPDF2
+import typer
+from rich.progress import Progress
+
+app = typer.Typer(
+    rich_markup_mode="markdown", help="Convert pdfs to txt"
+)
+
+
+@app.command()
+def convert(
+    slr_folder_path: Path = typer.Argument(
+        ...,
+        help="Path to pdfs folder to be converted",
+        dir_okay=True,
+        exists=True,
+    )
+):
+    files: list[str] = os.listdir(f"{slr_folder_path}\\pdfs")
+
+    with Progress() as progress:
+        convertion_progress = progress.add_task(
+            "[green]Converting...", total=len(files)
+        )
+        for index, file in enumerate(files):
+            paper_id: str = file.strip(".pdf")
+
+            with open(slr_folder_path / f"pdfs\\{file}", "rb") as f:
+                reader: PyPDF2.PdfReader = PyPDF2.PdfReader(f)
+                text: str = ""
+
+                for page_num in range(len(reader.pages)):
+                    page = reader.pages[page_num]
+                    text += page.extract_text()
+
+                with open(
+                    slr_folder_path / f"txts\\{paper_id}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f:
+                    f.write(text)
+
+            progress.update(
+                convertion_progress,
+                description=f"[green]Converting {index+1} of {len(files)}",
+                advance=1,
+                refresh=True,
+            )
