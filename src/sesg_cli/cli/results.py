0f80d67805a83c71ec42e04c92cf8356545fb895
Marialuisabcs <maria.lbcs@icloud.com>
2023-07-05 03:06:04 -0400

    feat: add progress, dynamic query creation, slr verification

--- src/sesg_cli/cli/results.py
+++ src/sesg_cli/cli/results.py
@@ -1,8 +1,14 @@
 import typer
-from pathlib import Path
 import pandas as pd
+
+from pathlib import Path
 from sesg_cli.database.models import SearchStringPerformance
 from sesg_cli.database import Session
+from sesg_cli.database.util.results_queries import ResultQuery
+from rich.progress import Progress
+
+_AVAILABLE_METRICS = ["`st_f1_score`", "`bsb_recall`", "`final_recall`"]
+_DEFAULT_METRICS = ["`st_precision`", "`st_recall`"]
 
 app = typer.Typer(
     rich_markup_mode="markdown", help="Get experiments' results"
@@ -15,32 +21,49 @@ def save(
             ...,
             help="Path where the results Excel file should be saved.",
             dir_okay=True,
-            exists=True
+            exists=True,
         ),
         slr: str = typer.Argument(
             ...,
             help="Name of the SLR the results will be extracted."
+        ),
+        metrics: list[str] = typer.Option(
+            default=None,
+            help="Bonus metrics to order the results and generate bonus top 10 lists. "
+                 f"Available metrics: {_AVAILABLE_METRICS} (outside the defaults: {_DEFAULT_METRICS})"
+        ),
+        algorithms: list[str] = typer.Option(
+            default=None,
+            help="Bonus algorithms to generate Excel tabs such as `lda` or `bt`. Important: a base query for the "
+                 "algorithm need to be previously implemented."
         )
 ):
-    #todo: add progress
-    #todo: verification for the slr passed
-
     print("Retrieving information from database...")
+
+    results_query: ResultQuery = ResultQuery(slr, metrics, algorithms)
+
     with Session() as session:
-        results = SearchStringPerformance.get_results(slr, session)
+        results = SearchStringPerformance.get_results(results_query, session)
 
     excel_writer = pd.ExcelWriter(path / f"{slr}.xlsx", engine='xlsxwriter')
 
-    # todo: apply some styles
-    # excel_writer_book = excel_writer.book
-    # header_format = excel_writer_book.add_format({'bold': True, 'bg_color': '#808080'})
+    with Progress() as progress:
+        saving_progress = progress.add_task(
+            "[green]Saving...", total=len(results)
+        )
+        with excel_writer:
+            for i, (key, result) in enumerate(results.items()):
+                df = pd.DataFrame(data=result['data'], columns=result['columns'])
+                df.to_excel(excel_writer=excel_writer, sheet_name=key, index=False)
 
-    with excel_writer:
-        for key, result in results.items():
-            df = pd.DataFrame(data=result['data'], columns=result['columns'])
-            df.to_excel(excel_writer=excel_writer, sheet_name=key, index=False)
+                for column in df:
+                    max_col_width = max(df[column].astype(str).map(len).max(), len(column))
+                    col_idx = df.columns.get_loc(column)
+                    excel_writer.sheets[key].set_column(col_idx, col_idx, max_col_width)
 
-            for column in df:
-                max_col_width = max(df[column].astype(str).map(len).max(), len(column))
-                col_idx = df.columns.get_loc(column)
-                excel_writer.sheets[key].set_column(col_idx, col_idx, max_col_width)
+                progress.update(
+                    saving_progress,
+                    description=f"[green]Saving {i + 1} of {len(results)}",
+                    advance=1,
+                    refresh=True,
+                )
