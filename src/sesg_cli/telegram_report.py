69c39b06cdcb2dd249382008622e530dbc480c64
Marialuisabcs <maria.lbcs@icloud.com>
2024-03-02 20:59:12 -0400

    fix: fix typos and type cast needed params

--- src/sesg_cli/telegram_report.py
+++ src/sesg_cli/telegram_report.py
@@ -1,16 +1,19 @@
 import os
-
-from telebot import TeleBot
-from typing import NoReturn
-from dotenv import load_dotenv
 from datetime import datetime
+
+from dotenv import load_dotenv
+from telebot import TeleBot
+
 
 load_dotenv()
 
 
 class TelegramReport:
-    def __init__(self, slr_name: str, experiment_name: str):
-        self._sesg_checkpoint_bot = TeleBot(token=os.environ.get('TELEGRAM_TOKEN'), parse_mode="HTML")
+    def __init__(self, slr_name: str, experiment_name: str, strategies: list[tuple[str, str]]):
+        self._sesg_checkpoint_bot = TeleBot(
+            token=str(os.environ.get('TELEGRAM_TOKEN')),
+            parse_mode="HTML"
+        )
         self._chat_id = os.environ.get('TELEGRAM_CHAT_ID')
 
         self.slr_name: str = slr_name
@@ -35,7 +38,7 @@ class TelegramReport:
 
         return hours, minutes, seconds
 
-    def send_new_execution_report(self) -> NoReturn:
+    def send_new_execution_report(self) -> None:
         """
         Report for a new experiment running. (`sesg experiment start`)
         """
@@ -45,42 +48,45 @@ class TelegramReport:
                   f"<b>Datetime</b>: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n" \
                   f"<b>PC specs</b>: {os.environ.get('PC_SPECS')}"
 
-        self._sesg_checkpoint_bot.send_message(chat_id=self._chat_id, text=message)
+        self._sesg_checkpoint_bot.send_message(
+            chat_id=str(self._chat_id), text=message)
 
-    def send_new_strategy_start_report(self, strategy: str) -> NoReturn:
+    def send_new_strategy_start_report(self, strategy: str) -> None:
         """
         Report for a new strategy starting its execution.
 
         Args:
-            strategy: wich strategy is starting. (e.g., lda, bertopic)
+            strategy: which strategy is starting. (e.g., lda, bertopic)
         """
         message = f"\U0001F7E2Starting <b>{strategy}</b>\U0001F7E2\n\n" \
                   f"<b>Experiment</b>: {self.experiment_name}\n" \
                   f"<b>Slr</b>: {self.slr_name}\n" \
                   f"<b>Percentage</b>: 0%\n"
 
-        self._sesg_checkpoint_bot.send_message(chat_id=self._chat_id, text=message)
+        self._sesg_checkpoint_bot.send_message(
+            chat_id=str(self._chat_id), text=message)
 
-    def send_progress_report(self, strategy: str, percentage: int, exec_time: float) -> NoReturn:
+    def send_progress_report(self, strategy: str, percentage: int, exec_time: float) -> None:
         """
         Report of the experiment progress (it's triggered every quarter of the total execution
         of each strategy).
 
         Args:
-            strategy: wich strategy is being executed.
-            percentage: what percentege the execution at
+            strategy: which strategy is being executed.
+            percentage: what percentage the execution at
             exec_time: total execution time passed.
         """
         hours, minutes, seconds = self.get_execution_time(exec_time)
-        message = f"\U0001F7E1Runing <b>{strategy}</b>...\U0001F7E1\n\n" \
+        message = f"\U0001F7E1Running <b>{strategy}</b>...\U0001F7E1\n\n" \
                   f"<b>Experiment</b>: {self.experiment_name}\n" \
                   f"<b>Slr</b>: {self.slr_name}\n" \
                   f"<b>Percentage</b>: {percentage}%\n" \
                   f"<b>Current execution time</b>: {hours}:{minutes}:{seconds}\n"
 
-        self._sesg_checkpoint_bot.send_message(chat_id=self._chat_id, text=message)
+        self._sesg_checkpoint_bot.send_message(
+            chat_id=str(self._chat_id), text=message)
 
-    def send_finish_report(self, exec_time: float) -> NoReturn:
+    def send_finish_report(self, exec_time: float) -> None:
         """
         End of the execution report.
 
@@ -93,9 +99,10 @@ class TelegramReport:
                   f"<b>Datetime</b>:{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n" \
                   f"<b>Execution total time</b>: {hours}:{minutes}:{seconds}\n"
 
-        self._sesg_checkpoint_bot.send_message(chat_id=self._chat_id, text=message)
+        self._sesg_checkpoint_bot.send_message(
+            chat_id=str(self._chat_id), text=message)
 
-    def send_error_report(self, error_message: str) -> NoReturn:
+    def send_error_report(self, error_message: str) -> None:
         """
         Error report.
         Args:
@@ -106,4 +113,5 @@ class TelegramReport:
                   f"<b>Datetime</b>: {datetime.now().strftime('%d-%m-%Y %H:%M:%S')}\n" \
                   f"<b>Error message</b>: {error_message}\n"
 
-        self._sesg_checkpoint_bot.send_message(chat_id=self._chat_id, text=message)
+        self._sesg_checkpoint_bot.send_message(
+            chat_id=str(self._chat_id), text=message)
