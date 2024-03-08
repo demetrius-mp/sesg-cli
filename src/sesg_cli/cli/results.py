618d4b1669c6e819d5f5c4f20df540dca2633a21
Marialuisabcs <maria.lbcs@icloud.com>
2024-03-07 20:27:04 -0400

    refactor: generate results excel file for multiple strategies combinations

--- src/sesg_cli/cli/results.py
+++ src/sesg_cli/cli/results.py
@@ -1,18 +1,17 @@
-import typer
-import pandas as pd
-
+from itertools import product
 from pathlib import Path
-from rich.progress import Progress
-from typing import NoReturn
 
-from sesg_cli.database.models import SearchStringPerformance
+import pandas as pd
+import typer
+from rich.progress import Progress
+
 from sesg_cli.database import Session
-from sesg_cli.database.util.results_queries import ResultQuery
-from sesg_cli.database.models import SLR
+from sesg_cli.database.models import SLR, SearchStringPerformance
+from sesg_cli.database.util.results_queries import ResultQuery, SideQueries
+
 
 _AVAILABLE_METRICS = ["start_set_f1_score", "bsb_recall", "sb_recall"]
 _DEFAULT_METRICS = ["start_set_precision", "start_set_recall"]
-_IMPLEMENTED_ALGORITHMS = ["lda", "bt"]
 
 app = typer.Typer(
     rich_markup_mode="markdown", help="Get experiments' results."
@@ -23,26 +22,20 @@ class InvalidMetric(Exception):
     """The metric passed as a parameter is not valid"""
 
 
-class InvalidAlgorithm(Exception):
-    """The algorithm passed as a parameter is not valid"""
-
-
-def verify_metrics_and_algorithms(metrics: list[str] | None, algorithms: list[str] | None) -> NoReturn:
+def verify_metrics(metrics: list[str] | None) -> None:
     if not set(metrics or []).issubset(set(_AVAILABLE_METRICS)):
         raise InvalidMetric()
 
-    if not set(algorithms or []).issubset(set(_IMPLEMENTED_ALGORITHMS)):
-        raise InvalidAlgorithm()
 
-
-def adjust_max_col_width(df: pd.DataFrame, writer: pd.ExcelWriter, sheet_name: str):
+def _adjust_max_col_width(df: pd.DataFrame, writer: pd.ExcelWriter, sheet_name: str):
     for column in df:
-        max_col_width = max(df[column].astype(str).map(len).max(), len(column))
+        max_col_width = max(df[column].astype(str).map(
+            len).max(), len(column))  # type: ignore
         col_idx = df.columns.get_loc(column)
         writer.sheets[sheet_name].set_column(col_idx, col_idx, max_col_width)
 
 
-def graph_tab(slr_name: str, excel_writer: pd.ExcelWriter):
+def _graph_tab(slr_name: str, excel_writer: pd.ExcelWriter):
     sheet_name = 'graph_info'
 
     with Session() as session:
@@ -59,50 +52,18 @@ def graph_tab(slr_name: str, excel_writer: pd.ExcelWriter):
     excel_writer.sheets[sheet_name].set_column(0, 1, 25)
 
 
-def save_xlsx(excel_writer: pd.ExcelWriter, results: dict[str, dict], slr: str):
-    with Progress() as progress:
-        saving_progress = progress.add_task(
-            "[green]Saving...", total=len(results)
-        )
-        with excel_writer:
-            for i, (key, result) in enumerate(results.items()):
-                df = pd.DataFrame(data=result['data'], columns=result['columns'])
-
-                if 'name' in df.columns:
-                    cols = df.columns.tolist()
-                    cols.remove('name')
-                    cols.insert(0, 'name')
-                    df = df[cols]
-
-                df.to_excel(excel_writer=excel_writer, sheet_name=key, index=False)
-                adjust_max_col_width(df, excel_writer, key)
-
-                progress.update(
-                    saving_progress,
-                    description=f"[green]Saving {i + 1} of {len(results)}",
-                    advance=1,
-                    refresh=True,
-                )
-
-            overall_results = {key: value for key, value in results.items() if key in _IMPLEMENTED_ALGORITHMS}
-            statistics_tab(overall_results, excel_writer)
-            graph_tab(slr, excel_writer)
-
-        progress.remove_task(saving_progress)
-
-
-def statistics_tab(results: dict[str, dict], excel_writer: pd.ExcelWriter) -> NoReturn:
+def _statistics_tab(results: dict[str, dict], excel_writer: pd.ExcelWriter, strategies: list[str]) -> None:
     root_cols = ['start_set_precision', 'start_set_recall', 'start_set_f1_score',
                  'bsb_recall', 'sb_recall', 'n_scopus_results']
     max_cols_highlight = ['mean_start_set_precision', 'mean_start_set_recall', 'mean_start_set_f1_score',
                           'mean_bsb_recall', 'mean_sb_recall']
 
-    idx = list()
+    idx = []
     for col in root_cols:
         idx.append(f'mean_{col}')
         idx.append(f'stdev_{col}')
 
-    stats_df = pd.DataFrame(columns=_IMPLEMENTED_ALGORITHMS, index=idx)
+    stats_df = pd.DataFrame(columns=strategies, index=idx)
 
     for key, result in results.items():
         temp_df = pd.DataFrame(data=result['data'], columns=result['columns'])
@@ -119,10 +80,81 @@ def statistics_tab(results: dict[str, dict], excel_writer: pd.ExcelWriter) -> No
         .highlight_min(subset=['mean_n_scopus_results'], color='#8aeda4')
 
     stats_df = stats_df.reset_index()
-    stats_df.rename(columns={'index': 'algorithm'}, inplace=True)
+    stats_df.rename(columns={'index': 'strategies'}, inplace=True)
 
     style_stats.to_excel(excel_writer=excel_writer, sheet_name='stats')
-    adjust_max_col_width(stats_df, excel_writer, 'stats')
+    _adjust_max_col_width(stats_df, excel_writer, 'stats')
+
+
+def save_xlsx(excel_writer: pd.ExcelWriter, results: dict[str, dict], slr: str, strategies: list[str]):
+    with Progress() as progress:
+        saving_progress = progress.add_task(
+            "[green]Saving...", total=len(results)
+        )
+        with excel_writer:
+            for i, (key, result) in enumerate(results.items()):
+                df = pd.DataFrame(
+                    data=result['data'], columns=result['columns'])
+
+                if 'name' in df.columns:
+                    cols = df.columns.tolist()
+                    cols.remove('name')
+                    cols.insert(0, 'name')
+                    df = df[cols]
+
+                df.to_excel(excel_writer=excel_writer,
+                            sheet_name=key, index=False)
+                _adjust_max_col_width(df, excel_writer, key)
+
+                progress.update(
+                    saving_progress,
+                    description=f"[green]Saving {i + 1} of {len(results)}",
+                    advance=1,
+                    refresh=True,
+                )
+
+            overall_results = {
+                key: value for key, value in results.items() if key in strategies}
+            _statistics_tab(overall_results, excel_writer, strategies)
+            _graph_tab(slr, excel_writer)
+
+        progress.remove_task(saving_progress)
+
+
+def get_results(slr: str, metrics: list[str]) -> tuple[dict, list[str]]:
+    strategies_used_queries = SideQueries.get_strategies_used_query(slr)
+    check_review_query = SideQueries.get_check_review_query(slr)
+
+    with Session() as session:
+        strategies_used = SearchStringPerformance.get_strategies_used(
+            strategies_used_queries, check_review_query, session)
+
+        results: dict = {}
+        strategies: list[str] = []
+
+        for strategies_set in product(*strategies_used.values()):
+            tes = strategies_set[0][0]
+            sws = strategies_set[1][0]
+
+            result_query: ResultQuery = ResultQuery(
+                slr=slr,
+                tes=tes,
+                ews=sws,
+                bonus_metrics=metrics,
+            )
+
+            queries = result_query.get_queries()
+
+            results.update(SearchStringPerformance.get_results(
+                queries, session))
+
+            strategies.append(f"{tes}-{sws}")
+
+        results.update(SearchStringPerformance.get_results(
+            result_query.get_qgs_query(),
+            session))
+
+    return results, strategies
 
 
 @app.command(help='Creates a Excel file based on the given Path and SLR.')
@@ -144,70 +176,13 @@ def save(
                  f"(outside the defaults: {[f'`{i}`' for i in _DEFAULT_METRICS]})",
             show_default=False
         ),
-        algorithms: list[str] = typer.Option(
-            default=None,
-            help=f"Bonus algorithms to generate Excel (outside the defaults: {[f'`{i}`' for i in _IMPLEMENTED_ALGORITHMS]}). "
-                 f"Important: a base query for the algorithm need to be previously implemented.",
-            hidden=True
-        )
 ):
-    verify_metrics_and_algorithms(metrics, algorithms)
+    verify_metrics(metrics)
 
     print("Retrieving information from database...")
 
-    result_query: ResultQuery = ResultQuery(slr, metrics, algorithms)
-    queries = result_query.get_queries()
-
-    with Session() as session:
-        results = SearchStringPerformance.get_results(queries, result_query.check_review, session)
+    results, strategies = get_results(slr, metrics)
 
     excel_writer = pd.ExcelWriter(path / f"{slr}.xlsx", engine='xlsxwriter')
 
-    save_xlsx(excel_writer, results, slr)
-
-
-@app.command(help='Creates a Excel file with the best `top` '
-                  'results from each experiment based on the given Path and SLR.')
-def save_by_row(
-        path: Path = typer.Argument(
-            ...,
-            help="Path to the **folder** where the results Excel file should be saved",
-            dir_okay=True,
-            exists=True,
-        ),
-        slr: str = typer.Argument(
-            ...,
-            help="Name of the SLR the results will be extracted."
-        ),
-        top: int = typer.Option(
-            default=10,
-            help="Number of best results per experiment to be retrieved.",
-        ),
-        metrics: list[str] = typer.Option(
-            default=None,
-            help="Bonus metrics to order the results and generate bonus lists. "
-                 f"Available metrics: {[f'`{i}`' for i in _AVAILABLE_METRICS]} "
-                 f"(outside the defaults: {[f'`{i}`' for i in _DEFAULT_METRICS]})",
-            show_default=False
-        ),
-        algorithms: list[str] = typer.Option(
-            default=None,
-            help=f"Bonus algorithms to generate Excel sheets "
-                 f"(outside the defaults: {[f'`{i}`' for i in _IMPLEMENTED_ALGORITHMS]}). "
-                 f"Important: a base query for the algorithm need to be previously implemented.",
-            hidden=True
-        )
-):
-    verify_metrics_and_algorithms(metrics, algorithms)
-
-    print("Retrieving information from database...")
-
-    result_query: ResultQuery = ResultQuery(slr, metrics, algorithms, top)
-    queries = result_query.get_queries_by_row()
-
-    with Session() as session:
-        results = SearchStringPerformance.get_results(queries, result_query.check_review, session)
-
-    excel_writer = pd.ExcelWriter(path / f"{slr}_top_per_exp.xlsx", engine='xlsxwriter')
-
-    save_xlsx(excel_writer, results, slr)
+    save_xlsx(excel_writer, results, slr, strategies)
--- src/sesg_cli/database/models/search_string_performance.py
+++ src/sesg_cli/database/models/search_string_performance.py
@@ -1,20 +1,11 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import (
-    Float,
-    ForeignKey,
-    Integer,
-    text
-)
-from sqlalchemy.orm import (
-    Mapped,
-    mapped_column,
-    relationship,
-    Session
-)
+from sqlalchemy import Float, ForeignKey, Integer, text
+from sqlalchemy.orm import Mapped, Session, mapped_column, relationship
 
 from .association_tables import gs_in_bsb, gs_in_sb, gs_in_scopus, qgs_in_scopus
 from .base import Base
+
 
 if TYPE_CHECKING:
     from .search_string import SearchString
@@ -103,7 +94,10 @@ class SearchStringPerformance(Base):
         )
 
     @staticmethod
-    def get_results(queries: dict[str, str], check_review_query: str, session: Session) -> dict[str, dict]:
+    def get_results(
+        queries: dict[str, str],
+        session: Session
+    ) -> dict[str, dict]:
         """
         Responsible for retrieving all the data needed to construct a results Excel file.
 
@@ -117,16 +111,41 @@ class SearchStringPerformance(Base):
                             'data': all the Rows resulting of the query}}
 
         """
-        results: dict = dict()
+        results: dict = {}
 
-        review_exists: bool = session.execute(text(check_review_query)).scalar()
+        for query_name, query in queries.items():
+            cursor = session.execute(text(query))
+            exec_results = cursor.fetchall()
+            results[query_name] = {'columns': tuple(
+                cursor.keys()), 'data': exec_results}
 
-        if not review_exists:
+        return results
+
+    @staticmethod
+    def get_strategies_used(
+        queries: dict[str, str],
+        check_review_query: str,
+        session: Session
+    ) -> dict[str, dict]:
+        """_summary_
+
+        Args:
+            queries: sql queries to be executed.
+            check_review_query: query to check if the review exists.
+            session: database connection session.
+        Raises:
+            ReviewDoesNotExist: If the review does not exist in the database.
+        Returns:
+            Query results in a dictionary.
+        """
+        results: dict = {}
+
+        if not bool(session.execute(text(check_review_query)).scalar()):
             raise ReviewDoesNotExist()
 
         for query_name, query in queries.items():
             cursor = session.execute(text(query))
             exec_results = cursor.fetchall()
-            results[query_name] = {'columns': tuple(cursor.keys()), 'data': exec_results}
+            results[query_name] = [*exec_results]
 
         return results
